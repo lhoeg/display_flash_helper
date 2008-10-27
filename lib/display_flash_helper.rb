83f7b711b73514b3aba08127d22ef6412151afe9
Lars E. Hoeg <leh@lenio.dk>
2008-10-27 12:13:01 +0100

    Added configuration of flash message types, classes and ids

--- lib/display_flash_helper.rb
+++ lib/display_flash_helper.rb
@@ -1,13 +1,22 @@
 module DisplayFlashHelper
-  def display_flash
-    flash_types = [:error, :warning, :notice ]
+  # global options for displaying flash messages
+  @@flash_options = {
+    :flash_types    => [:error, :warning, :notice],
+    :error_class    => 'error',
+    :warning_class  => 'warning',
+    :notice_class   => 'notice',
+    :info_class     => 'info',
+    :flash_id       => 'flash'
+  }
+  mattr_accessor :flash_types
 
-    messages = ((flash_types & flash.keys).collect do |key|
-      content_tag(:div, flash[key], :class => "#{key}")
+  def display_flash
+    messages = ((@@flash_options[:flash_types] & flash.keys).collect do |key|
+      content_tag(:div, flash[key], :class => @@flash_options["#{key}_class".to_sym])
     end.join("\n"))
-    
+
     if messages.size > 0
-      content_tag(:div, messages, :id => "flash")
+      content_tag(:div, messages, :id => @@flash_options[:flash_id])
     else
       ""
     end
